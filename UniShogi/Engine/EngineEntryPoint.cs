8c6c0febd9140bde9ff49925ff53a6334e73f158
tsubasa-alife <tsubasa.robotics@gmail.com>
2024-09-03 00:11:26 +0900

    エンジンコードからテストコードを分離

--- UniShogi/Engine/EngineEntryPoint.cs
+++ UniShogi/Engine/EngineEntryPoint.cs
@@ -12,39 +12,11 @@ namespace UniShogi
 		public const string IgnoreBookPlay = "IgnoreBookPlay";
 
 		private ILogger _logger;
+		private Position _position;
 		
 		public void Initialize(ILogger logger)
 		{
 			_logger = logger;
-			Setup();
-		}
-
-		private void Setup()
-		{
-			var pos = new Position("lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1"); // 平手
-
-			_logger.Log(pos.Sfen());
-
-			_logger.Log(pos.Pretty());
-			pos.DoMove(Usi.ParseMove("2g2f"));
-			_logger.Log(pos.Pretty());
-
-			_logger.Log(pos.Player == Color.White); // true
-
-			pos.UndoMove();
-			
-			_logger.Log(pos.InCheck()); // false
-			_logger.Log(pos.IsMated()); // false
-			_logger.Log(pos.IsLegalMove(Usi.ParseMove("7g7f"))); // true
-			_logger.Log(pos.CheckRepetition() == Repetition.None); // true
-			_logger.Log(pos.CanDeclareWin()); // false
-
-			foreach (var m in Movegen.GenerateMoves(pos))
-			{
-				_logger.Log(m.ToUsi());
-			}
-			
-			_logger.Log("Setup Completed.");
 		}
 
 		public void ReceiveUsiCommand(string command)
@@ -60,6 +32,17 @@ namespace UniShogi
 					_logger.Log($"option name {BookFile} type string default user_book1.db");
 					_logger.Log($"option name {IgnoreBookPlay} type check default true");
 					_logger.Log("usiok");
+					break;
+				case "isready":
+					_logger.Log("readyok");
+					break;
+				case "usinewgame":
+					break;
+				case "position":
+					break;
+				case "go":
+					break;
+				case "quit":
 					break;
 				default:
 					_logger.Log($"info string Unsupported command: command={command}");
--- UniShogi/Program.cs
+++ UniShogi/Program.cs
@@ -3,6 +3,35 @@
 var logger = new ConsoleLog();
 var engine = new EngineEntryPoint();
 
+var pos = new Position("lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1"); // 平手
+
+logger.Log(pos.Sfen());
+
+logger.Log(pos.Pretty());
+
+pos.DoMove(Usi.ParseMove("2g2f"));
+
+logger.Log(pos.Pretty());
+
+logger.Log(pos.Player == Color.White); // true
+
+pos.UndoMove();
+			
+logger.Log(pos.InCheck()); // false
+logger.Log(pos.IsMated()); // false
+logger.Log(pos.IsLegalMove(Usi.ParseMove("7g7f"))); // true
+logger.Log(pos.CheckRepetition() == Repetition.None); // true
+logger.Log(pos.CanDeclareWin()); // false
+
+foreach (var m in Movegen.GenerateMoves(pos))
+{
+	logger.Log(m.ToUsi());
+}
+
+logger.Log(Movegen.GenerateMoves(pos).Count); // 31
+			
+logger.Log("Test Completed.");
+
 engine.Initialize(logger);
 
 string commandLine = null;
